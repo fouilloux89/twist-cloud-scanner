6652aa49ff987df5bb6c99ea1339407deecfe66e
fouilloux89 <philippe.fouilloux89@laposte.net>
2025-04-20 21:43:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask
 import os
 
+# Supposons que tu as une fonction dans un autre fichier
+from twist_utils import scanner  
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Twist Scanner en ligne 🚀"
+
+@app.route('/scan')
+def lancer_scan():
+    resultat = scanner()
+    return f"Résultat du scan : {resultat}"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
